d24753ff9a7f5ab1f234f0644675795f8ec6b107
MJahidH <m.jahid.h@hotmail.com>
2024-06-17 12:45:49 +0100

    bankaccount class has been creaed with a function to deposoit money ontop of the balance

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,7 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-System.out.print("Hello and welcome!");
+       System.out.print("Hello and welcome!");
 
 
+    }
+}
+
+class BankAccount {
+    int balance;
+    int previousTransactions;
+    String customerName;
+    String customerID;
+
+    void deposit (int amount) {
+        balance += amount;
     }
 }
