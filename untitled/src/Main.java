c0ddb112bb24cda895471634d9fd9a054c9e5c79
MJahidH <m.jahid.h@hotmail.com>
2024-06-19 16:38:52 +0100

    show menu function has been created. do while loop created with switch statemtns insice it, cases are referred to whatever key the user has pressed. Error handling has been created, represented by the default switch statment.

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
        System.out.print("Hello and welcome!");
@@ -41,7 +43,71 @@ class BankAccount {
     }
 
     void showMenu() {
+       char option = '\0';
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Welcome" +customerName);
+        System.out.println("Your ID is " +customerID);
+        System.out.println("\n");
+        System.out.println("A. Check balance");
+        System.out.println("B. Deposit");
+        System.out.println("C. Withdraw");
+        System.out.println("D. Previous transaction");
+        System.out.println("E. Exit");
+
+        do {
+            System.out.println("-----------------------------------------------");
+            System.out.println("Enter an option");
+            System.out.println("-----------------------------------------------");
+            option = scanner.next().charAt(0);
+            System.out.println("\n");
+
+            switch (option) {
+
+                case 'A':
+                    System.out.println("-----------------------------------------------");
+                    System.out.println("Balance: " +balance);
+                    System.out.println("-----------------------------------------------");
+                    System.out.println("\n");
+                    break;
+
+                case 'B':
+                    System.out.println("-----------------------------------------------");
+                    System.out.println("Enter an amount to deposit: ");
+                    System.out.println("-----------------------------------------------");
+                    int depositAmount = scanner.nextInt();
+                    deposit(depositAmount);
+                    System.out.println("\n");
+                    break;
+
+                case 'C':
+                    System.out.println("-----------------------------------------------");
+                    System.out.println("Enter an amount to withdraw:");
+                    System.out.println("-----------------------------------------------");
+                    int withdrawAmount = scanner.nextInt();
+                    withdraw(withdrawAmount);
+                    System.out.println("\n");
+                    break;
+
+                case 'D':
+                    System.out.println("-----------------------------------------------");
+                    getPreviousTransaction();;
+                    System.out.println("-----------------------------------------------");
+                    System.out.println("\n");
+                    break;
+
+                default:
+                    System.out.println("invalid option! Please enter again");
+                    break;
+
+
+
+            }
+
+
+        }while (option != 'E');
+
+        System.out.println("Thank you for using our services.");
     }
 
 }
